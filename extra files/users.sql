82609dbae4df3f914448f6218c54aecd988a1d60
Connor McLeod <connorbmcleod@gmail.com>
2016-02-04 12:31:46 -0800

    sql file update

--- extra files/users.sql
+++ extra files/users.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 02, 2016 at 11:24 PM
+-- Generation Time: Feb 04, 2016 at 09:29 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.5.28
 
@@ -23,33 +23,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users2`
+-- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users2` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `password` char(64) NOT NULL,
   `salt` char(16) NOT NULL,
-  `email` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `email` varchar(255) NOT NULL,
+  `usertype` int(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users2`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users2` (`id`, `firstname`, `lastname`, `password`, `salt`, `email`) VALUES
-(1, 'Connor', 'McLeod', '532800bd29f2de6a3acac179d507cf50006eb0590af35eeb62a6a955f80cac82', '18ce0b215767f37b', 'connorbmcleod@gmail.com');
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `password`, `salt`, `email`, `usertype`) VALUES
+(1, 'Connor', 'McLeod', '532800bd29f2de6a3acac179d507cf50006eb0590af35eeb62a6a955f80cac82', '18ce0b215767f37b', 'connorbmcleod@gmail.com', 2);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `users2`
+-- Indexes for table `users`
 --
-ALTER TABLE `users2`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -57,10 +58,10 @@ ALTER TABLE `users2`
 --
 
 --
--- AUTO_INCREMENT for table `users2`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `users2`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
