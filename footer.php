6be79da6ee47d0d4c80b333d7289165f2ee854ab
Fedorchuk <kislefedorchuk@gmail.com>
2016-03-03 13:26:01 -0800

    Changing Footer
    Almost final, fuck it.

--- footer.php
+++ footer.php
@@ -1,18 +1,40 @@
     <div class="footer">
-            <div class="footer-col" id="footer-one">
-            </div>
 
-             <div class="footer-col" id="footer-two">
-                 <p>#555 Seymour Street | info@welearn.ca</p>
-                <p>2016 | DACOKYE DESIGN</p>
-             </div>
+<div id="foot_title">
+    <h2 id="foot_welearn">WeLearn</h2>
+        <div id="social_media_btns">
+            <p class="sm">Twitter</p>
+            <p class="sm">Instagram</p>
+            <p class="sm">Facebook</p>
+            <p class="sm contact_foot">Contact Us</p>
+        </div>
 
-             <div class="footer-col" id="footer-three">
-                <ul id="social-media">
+
+
+</div>
+
+<!-- <div id="social_media_btns">
+            <p class="sm">Twitter</p>
+            <p class="sm">Instagram</p>
+            <p class="sm">Facebook</p>
+</div> -->
+
+<!-- <div id="foot_title_2">
+    <h2 id="foot_codakye">Codakye</h2>
+        <div id="social_media_btns_2">
+            <p class="sm">Twitter</p>
+            <p class="sm">Instagram</p>
+            <p class="sm">Facebook</p>
+        </div>
+
+</div> -->
+
+            <!-- <div id="social-media">
+                <ul>
                     <li><a href="#"><img src="images/facebook-square.png" width="30px"></a></li>
                     <li><a href="#"><img src="images/instagram.png" width="30px"></a></li>
                     <li><a href="#"><img src="images/twitter.png" width="30px"></a></li>
                     <li><a href="#"><img src="images/googleplus-square.png" width="30px"></a></li>
                 </ul>
-            </div>
+            </div> -->
     </div>
